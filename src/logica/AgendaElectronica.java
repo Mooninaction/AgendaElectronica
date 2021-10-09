d0bc900b5e9ce373203e402c18465c852aec50ff
Mooninaction <lunasauri.ls@gmail.com>
2021-10-09 20:00:24 -0300

    probandoPad

---
+++ src/logica/AgendaElectronica.java
@@ -0,0 +1,18 @@
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author Luna
+ */
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+       Pantalla panta = new Pantalla();
+       panta.setVisible(true);
+       panta.setLocationRelativeTo(null);
+    }
+    
+}
